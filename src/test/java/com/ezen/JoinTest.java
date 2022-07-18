43ba4ad218180c2b0f6be39e41dc412838377995
JiHoon <raytoy@daum.net>
2022-07-18 17:30:52 +0900

    Issues#2 회원가입 구현 완료

---
+++ src/test/java/com/ezen/JoinTest.java
@@ -0,0 +1,20 @@
+package com.ezen;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JoinTest {
+	
+	@Test
+	public void JoinTest() {
+		System.out.println("issue#2 회원가입 구현 완료");
+		System.out.println("회원 가입: ");
+		
+		
+	}
+
+}
