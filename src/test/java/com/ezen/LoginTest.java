5a6baf3964919ad7825a61a537448b497cd9f36d
JiHoon <raytoy@daum.net>
2022-07-18 17:27:34 +0900

    index와 Login 구현 완료

---
+++ src/test/java/com/ezen/LoginTest.java
@@ -0,0 +1,18 @@
+package com.ezen;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoginTest {
+
+	@Test
+	public void board() {
+		System.out.println("로그인 기능 구현 완료");
+	}
+
+	
+}
